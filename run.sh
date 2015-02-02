243a6b859cf5ced0ba4bcc480618ab73c8ab481f
Nadrieril <nadrieril@gmail.com>
2015-02-02 17:53:07 +0100

    Added dockerfile build files (from https://github.com/Nadrieril/bars)

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker build -t binetreseau/nat24h .
+docker run --name="nat24h-db" -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=django -d mysql
+docker run -p 8000:8000 --name="nat24h" --link="nat24h-db:mysqldb" binetreseau/nat24h
