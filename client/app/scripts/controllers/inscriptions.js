e670b970e2a31e072027780d17f22accbb3f9c36
Basile Bruneau <basilebruneau@gmail.com>
2015-02-08 15:23:25 +0100

    Add team creation/join

--- client/app/scripts/controllers/inscriptions.js
+++ client/app/scripts/controllers/inscriptions.js
@@ -13,6 +13,8 @@ angular.module('v24hApp')
 
       var Slotsubscription = $resource(APIURL + '/slotsubscription/:id/');
       var Slot = $resource(APIURL + '/slot/:id');
+      var Activity = $resource(APIURL + '/activity/:id');
+      var Team = $resource(APIURL + '/team/:id/', {id:'@id'}, {update: {method: 'PUT'}});
 
       var slots = Slot.query(function ()  {
           $scope.slots = {};
@@ -20,6 +22,15 @@ angular.module('v24hApp')
               $scope.slots[slots[i].id] = slots[i];
           }
       });
+      var activities = Activity.query(function ()  {
+          $scope.activities = {};
+          for (var i = 0; i < activities.length; i++) {
+              $scope.activities[activities[i].id] = activities[i];
+          }
+      });
+      $scope.teams = Team.query();
+      $scope.nteam = {activity: 1, name: ''};
+      $scope.jteam = {team: 1};
 
       function reloadSlotsubscriptions() {
           var slotsubscriptions = Slotsubscription.query({'user': auth.getUser().id}, function () {
@@ -37,5 +48,20 @@ angular.module('v24hApp')
       };
       $scope.unsubscribe = function (sid) {
           Slotsubscription.delete({id: sid}, reloadSlotsubscriptions);
-      }
+      };
+
+      $scope.createTeam = function () {
+          var nteam = new Team();
+          nteam.activity = $scope.nteam.activity;
+          nteam.name = $scope.nteam.name;
+          nteam.admin = auth.getUser().id;
+          nteam.members = [auth.getUser().id];
+          nteam.$save();
+      };
+      $scope.joinTeam = function () {
+          Team.get({id: $scope.jteam.team}, function (cteam) {
+              cteam.members.push(auth.getUser().id);
+              cteam.$update();
+          });
+      };
   });
