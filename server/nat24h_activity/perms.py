67640bca5c7d6c78d68c2b343c9c476f5afc7a8c
Nadrieril <nadrieril@gmail.com>
2015-02-10 18:32:23 +0100

    Team admins can delete members

--- server/nat24h_activity/perms.py
+++ server/nat24h_activity/perms.py
@@ -2,6 +2,7 @@ from permission.logics import AuthorPermissionLogic
 
 PERMISSION_LOGICS = (
     ('nat24h_activity.TeamSubscription', AuthorPermissionLogic(field_name='user', delete_permission=True)),
+    ('nat24h_activity.TeamSubscription', AuthorPermissionLogic(field_name='team__admin', delete_permission=True)),
     ('nat24h_activity.TimeSlotSubscription', AuthorPermissionLogic(field_name='user', delete_permission=True)),
     ('nat24h_activity.Team', AuthorPermissionLogic(field_name='admin', delete_permission=False)),
 )
