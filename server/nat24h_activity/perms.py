d35c8376f600b2bd905bf5e85de3d71ba4be6fbe
Nadrieril <nadrieril@gmail.com>
2015-02-10 17:49:13 +0100

    Users can unsubscribe from slots and teams

--- server/nat24h_activity/perms.py
+++ server/nat24h_activity/perms.py
@@ -1,7 +1,7 @@
 from permission.logics import AuthorPermissionLogic
 
 PERMISSION_LOGICS = (
-    ('nat24h_activity.TeamSubscription', AuthorPermissionLogic(field_name='user')),
-    ('nat24h_activity.TimeSlotSubscription', AuthorPermissionLogic(field_name='user')),
-    ('nat24h_activity.Team', AuthorPermissionLogic(field_name='admin')),
+    ('nat24h_activity.TeamSubscription', AuthorPermissionLogic(field_name='user', delete_permission=True)),
+    ('nat24h_activity.TimeSlotSubscription', AuthorPermissionLogic(field_name='user', delete_permission=True)),
+    ('nat24h_activity.Team', AuthorPermissionLogic(field_name='admin', delete_permission=False)),
 )
