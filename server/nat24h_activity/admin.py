df86e99f69e408d09a8483b3aa71634b25196cad
Nadrieril <nadrieril@gmail.com>
2015-02-10 20:13:31 +0100

    Bugfix: added TeamSubscription to admin

--- server/nat24h_activity/admin.py
+++ server/nat24h_activity/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from nat24h_activity.models import Activity, Team, TimeSlot, TimeSlotSubscription
+from nat24h_activity.models import Activity, Team, TeamSubscription, TimeSlot, TimeSlotSubscription
 
 admin.site.register(Activity)
 admin.site.register(Team)
+admin.site.register(TeamSubscription)
 admin.site.register(TimeSlot)
 admin.site.register(TimeSlotSubscription)
