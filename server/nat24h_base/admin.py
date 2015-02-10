b0c52f667151bfeeb80eb81ad6e89c2a40db35b9
Nadrieril <nadrieril@gmail.com>
2015-02-10 17:14:49 +0100

    Updated admin interface

--- server/nat24h_base/admin.py
+++ server/nat24h_base/admin.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import models
 from django.contrib import admin
 
 from nat24h_base.models import User, Profile, Group
 
-# admin.site.register(User)
+admin.site.register(User)
+admin.site.unregister(models.Group)
 admin.site.register(Profile)
 admin.site.register(Group)
