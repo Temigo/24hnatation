7e7f53c9d45bf624d8d459c69c11b6296e54868f
Nadrieril <nadrieril@gmail.com>
2015-02-10 14:09:11 +0100

    Bugfix: forgot migration

---
+++ server/nat24h_activity/migrations/0003_auto_20150210_1300.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('nat24h_activity', '0002_auto_20150202_1932'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TeamSubscription',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('team', models.ForeignKey(to='nat24h_activity.Team')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.RemoveField(
+            model_name='team',
+            name='members',
+        ),
+    ]
